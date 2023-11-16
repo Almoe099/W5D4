6a6a4203cb5decd0a30df214a7b90621438cef03
Jacob Tabor <jtabor214@gmail.com>
2023-11-16 16:21:14 -0500

    Finished Course class and User class

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -11,13 +11,23 @@
 #
 class Course < ApplicationRecord
 
-    # belongs_to :students
-    #     primary_key: :id,
-    #     foreign_key: :student_id,
-    #     class_name: :User
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
 
+    has_many :enrolled_students,
+        through: :enrollments,
+        source: :student
 
-
-
+    belongs_to :prerequisite,
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course
+    
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :User
 
 end
--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -18,6 +18,4 @@ class User < ApplicationRecord
         through: :enrollments,
         source: :course
 
-
-
 end
